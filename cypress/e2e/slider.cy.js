78faa313528dd164c313d7e89b5986148b87a6a4
SM4rtinho <sebastianmarut2@gmail.com>
2025-03-08 15:58:57 +0100

    test and hotjar added

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -15,4 +15,16 @@ describe('Swiper Gallery Test', function () {
     cy.wait(2000);
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
+  describe('Swiper Gallery Test', function () {
+    it('Checks if first slide contains "New York"', function () {
+      cy.visit('http://localhost:3000');
+      cy.get('.swiper-button-next').click();
+      cy.wait(2000);
+      cy.get('.swiper-button-next').click({ force: true });
+      cy.wait(2000);
+      cy.get('.swiper-button-next').click({ force: true });
+      cy.wait(2000);
+      cy.get('.swiper-slide-active').should('contain', 'Rome');
+    });
+  });
 });
